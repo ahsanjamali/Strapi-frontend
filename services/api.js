b2b2cb02c3fc6a17ba0b697fd353a32e8b3d2249
ahsanjamali <ahsanjamali7042@gmail.com>
2024-12-10 00:22:19 +0500

    Improvement 2

--- services/api.js
+++ services/api.js
@@ -1,4 +1,5 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+const API_URL =
+  process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337";
 
 export const getFeaturesData = async () => {
   const res = await fetch(`${API_URL}/api/features`);
